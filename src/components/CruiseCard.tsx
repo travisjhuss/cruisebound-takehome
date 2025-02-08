de8a233e80612a12de9c32c9fedc8c037a021da2
Travis Huss <travisjhuss@gmail.com>
2025-02-08 11:49:39 -0600

    connect data to card

--- src/components/CruiseCard.tsx
+++ src/components/CruiseCard.tsx
@@ -9,6 +9,7 @@ type CruiseData = {
     name: string;
     region: string;
     returnDate: string;
+    price: number;
     ship: {
         line: {
             name: string;
@@ -26,23 +27,23 @@ interface CruiseCardProps {
 }
 
 const CruiseCard: FC<CruiseCardProps> = ({cruiseData}) => {
-    const { departureDate, returnDate, name, region, duration, ship } = cruiseData;
+    const { departureDate, returnDate, name, region, duration, ship, price, itinerary } = cruiseData;
     const formattedDateRange = formatDateRange(departureDate, returnDate);
 
     return (
         <div className="flex flex-col max-w-4xl shadow-[5px_5px_10px_0_rgba(0,0,0,0.1)] rounded-xl w-full md:flex-row">
-            <div className="static">
+            <div className="static w-full md:max-h-full md:max-w-[250px]">
                 <div className="flex absolute text-white bg-black/75 mt-3 ml-3 px-2 py-1 rounded">
                     <span className="text-sm font-medium">
                         {formattedDateRange}
                     </span>
                 </div>
                 <Image
-                    src="https://res.cloudinary.com/cruisebound/image/upload/f_auto/v1656397931/production/small_msc_vi_item_76c6c4b53c.png"
+                    src={ship.image}
                     alt="Cruise Ship"
-                    width={300}
-                    height={350} 
-                    className="rounded-l-xl"
+                    width={500}
+                    height={550}
+                    className="rounded-t-xl md:rounded-l-xl md:rounded-tr-none object-cover"
                 />
             </div>
             <div className="flex flex-col w-full">
@@ -54,29 +55,39 @@ const CruiseCard: FC<CruiseCardProps> = ({cruiseData}) => {
                             <p className="text-sm font-medium text-gray-500">{duration} nights</p>
                             <div className="flex flex-row items-center">
                                 <Image src="/star.svg" alt="Star" width={16} height={16} />
-                                <p className="text-sm">{ship.rating}</p>
+                                <p className="text-sm pr-[4px]">{ship.rating}</p>
                                 <p className="text-xs text-gray-400">{ship.reviews} reviews</p>
                             </div>
                         </div>
-                        <div className="flex flex-row space-x-4">
-                            <p className="text-sm font-medium">Departure</p>
-                            <Image src="/arrowright.svg" alt="Star" width={16} height={16} />
+                        <div className="flex flex-row flex-wrap space-x-1">
+                            {itinerary.map((port, i) => {
+                                const city = port.split(',')[0];
+                                return (
+                                <div key={i} className="flex flex-row items-center space-x-1">
+                                    <p className="text-sm font-medium">{city}</p>
+                                    {i < itinerary.length - 1 &&
+                                        <Image src="/arrowright.svg" alt="right-arrow" width={12} height={12} />
+                                    }
+                                </div>
+                            )})}
                         </div>
                     </div>
-                    <div>
-                        <Image 
-                            src="https://res.cloudinary.com/cruisebound/image/upload/f_auto/v1649242239/production/line_logo_6_4bbd4246_eded_4690_bd5e_b184f4a64e82_0f9ac3171a.jpg"
-                            alt="Cruise Logo"
-                            width={100}
-                            height={100} 
-                            className="rounded-l-xl"
-                        />
+                    <div className="mt-4 min-w-[100px]">
+                        {ship.line.logo && 
+                            <Image 
+                                src={ship.line.logo}
+                                alt="Cruise Logo"
+                                width={100}
+                                height={100} 
+                                className="rounded-l-xl"
+                            />
+                        }
                     </div>
                 </div>
                 <div className="flex flex-row justify-end py-3 px-5 space-x-4 basis-1/3 bg-gray-50">
                     <div className="flex flex-col items-end">
                         <p className="text-xs font-medium text-gray-400">Interior from</p>
-                        <p className="text-lg font-medium">$1000</p>
+                        <p className="text-lg font-medium">${price.toLocaleString()}</p>
                     </div>
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 h-[44px] rounded">
                         See sailings
